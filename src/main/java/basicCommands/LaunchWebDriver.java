0f07f0db6d563db3d3a429d00a9e471ce8b239c8
Soumya Mishra <soumya_rm@outlook.com>
2021-07-23 21:15:45 +0530

    added robot command

--- src/main/java/basicCommands/LaunchWebDriver.java
+++ src/main/java/basicCommands/LaunchWebDriver.java
@@ -1,15 +1,19 @@
 package basicCommands;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.awt.*;
+import java.awt.event.KeyEvent;
 import java.util.concurrent.TimeUnit;
 
 public class LaunchWebDriver {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws AWTException, InterruptedException {
 
         // Command to launch webdriver
-        System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir")+"/chromedriver");
+        System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/chromedriver");
         WebDriver driver = new ChromeDriver();
 
         // Common waits and pre-requisites
@@ -18,17 +22,25 @@ public class LaunchWebDriver {
         driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 
         // to type address and launch
-        driver.get("https://www.github.com");
+        driver.get("https://www.google.com");
 
         //to get Title
         String title = driver.getTitle();
-        if(title.toLowerCase().contains("github")){
+        if (title.toLowerCase().contains("google")) {
             System.out.println("Title matched");
-        }
-        else{
+        } else {
             System.out.println("title mismatched");
         }
 
+        //to type in capitals
+        driver.findElement(By.name("q")).sendKeys(Keys.chord(Keys.SHIFT, "selenium"));
+        driver.findElement(By.name("q")).sendKeys(Keys.ENTER);
+
+        //Robot robot = new Robot();
+        //robot.keyPress(KeyEvent.VK_ENTER);
+
+
+        Thread.sleep(5000);
         //quit
         driver.quit();
     }
